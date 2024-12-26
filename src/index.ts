e6c3a6c05e22fb44d26d2dbcdf3027edfa42a5a2
LeTrungPhong <letrungphong22122004@gmail.com>
2024-12-27 00:37:20 +0700

    elements in pixi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Container, Sprite, Point, ParticleContainer, Graphics, TextStyle, Text, BitmapFont, BitmapText } from 'pixi.js'
+import { Application, Assets, BitmapFont, BitmapText, BlurFilter, Container, Graphics, Sprite, Text, TextStyle } from 'pixi.js'
 
 const app = new Application<HTMLCanvasElement>({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,81 +9,68 @@ const app = new Application<HTMLCanvasElement>({
 	height: 480
 });
 
-// const clampy: Sprite = Sprite.from("clampy.png");
+const container_1: Container = new Container();
+container_1.x = app.screen.width / 2;
+container_1.y = app.screen.height / 2;
+app.stage.addChild(container_1);
 
-// clampy.anchor.set(0.5);
+Assets.load("clampy.svg").then(() => {
+    const image_1: Sprite = Sprite.from("clampy.svg");
+    image_1.x = -image_1.width / 2;
+    image_1.y = -image_1.height / 2;
+    container_1.addChild(image_1);
 
-// clampy.x = app.screen.width / 2;
-// clampy.y = app.screen.height / 2;
+    const myBlurFilter = new BlurFilter();
+    image_1.filters = [myBlurFilter];
 
-// app.stage.addChild(clampy);
+    console.log(image_1.width + " " + image_1.height);
+});
 
+// (Container)
+// - addChild
+// - removehild
+// - children
+// - position, scale, skew
+// - rotation, angle
+// - width, height
+// - interactive, on, off
+// - getBounds
+// - destroy
 
-////  container
-const bigConty: Container = new Container();
-bigConty.scale.set(2); // You can use set and only one value to set x and y
-bigConty.position.x = 100;
-bigConty.y = 200; // this is a shortcut for .position.y and it also exists one for .position.x
-app.stage.addChild(bigConty);
+// (Particle Container)
+// const particleContainer_1: ParticleContainer = new ParticleContainer();
 
-const littleConty: Container = new Container();
-// position has a copy setter. It won't use your reference but copy the values from it!
-littleConty.position = new Point(300,200);
-bigConty.addChild(littleConty);
+// (Sprite)
+// - anchor set(0.5)
+// - tint
 
-const conty: Container = new Container();
-conty.x = 200;
-conty.y = 0;
-app.stage.addChild(conty);
+const graphy_1: Graphics = new Graphics();
 
+graphy_1.beginFill((255 << 16) + (100 << 8) + 50 , 1 );
+graphy_1.lineStyle(10, 0x00FF00);
+graphy_1.drawCircle(0, 0, 25);
+graphy_1.endFill();
+graphy_1.x = 100;
+graphy_1.y = 100;
 
-// sprite
-const clampy: Sprite = Sprite.from("clampy.svg");
-clampy.x = 100;
-clampy.y = 100;
-conty.addChild(clampy);
+app.stage.addChild(graphy_1);
 
-const clampy1: Sprite = Sprite.from("clampy.svg");
-clampy1.x = 50;
-clampy1.y = 50;
-conty.addChild(clampy1);
-conty.removeChild(clampy1);
+// (Graphics)
+// - clear
 
+// (Text)
 
-
-// partical Container
-const particleConty: ParticleContainer = new ParticleContainer();
-particleConty.addChild(Sprite.from("clampy.svg"));
-// Pretty much everything that worked on a Container will work with a ParticleContainer.
-
-
-// Graphics
-const graphy: Graphics = new Graphics();
-
-// we give instructions in order. begin fill, line style, draw circle, end filling
-graphy.beginFill(0xFF00FF);
-graphy.lineStyle(10, 0x00FF00);
-graphy.drawCircle(0, 0, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
-graphy.endFill();
-
-app.stage.addChild(graphy); //I can add it before setting position, nothing bad will happen.
-
-// Here we set it at 100,100
-graphy.x = 100;
-graphy.y = 100;
-
-// Text, TextStyle
 const styly: TextStyle = new TextStyle({
     align: "center",
     fill: "#754c24",
-    fontSize: 42
+    fontSize: 20
 });
-const texty: Text = new Text('私に気づいて先輩！', styly); // Text supports unicode!
-texty.text = "This is Text";
+const texty: Text = new Text('Welcome to my world！', styly);
+texty.text = "This is expensive to change, please do not abuse";
 
 app.stage.addChild(texty);
 
-//BitmapText
+
 BitmapFont.from("comic 32", {
     fill: "#ffffff", // White, will be colored later
     fontFamily: "Comic Sans MS",
@@ -99,9 +86,24 @@ const bitmapTexty: BitmapText = new BitmapText("I love baking, my family, and my
     });
 
 bitmapTexty.text = "This is cheap";
-bitmapTexty.text = "Change it";
+bitmapTexty.text = "Change it as much as you want";
+bitmapTexty.x = 0;
+bitmapTexty.y = 15;
 
 app.stage.addChild(bitmapTexty);
+
+// Particles
+
+
+
+
+
+
+
+
+
+
+
 
 
 
