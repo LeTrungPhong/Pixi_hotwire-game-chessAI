122278e7e24d98494024c3ff509635e145c97bcb
LeTrungPhong <letrungphong22122004@gmail.com>
2025-01-18 14:35:04 +0700

    feat: override move of pawn_piece

--- src/models/pawn_piece.ts
+++ src/models/pawn_piece.ts
@@ -7,32 +7,32 @@ export default class Pawn extends Piece {
         super(name, value, moved, scaleScene, texture);
     }
 
-    public override move(boardState: any, startX: number, startY: number): { indexX: number; indexY: number; }[] {
+    public override move(boardState: any, startY: number, startX: number): { indexX: number; indexY: number; }[] {
         var positiveMove: { indexX: number; indexY: number; }[] = [];
         var endY: number = startY;
         if (Math.abs(this.getValue()) == 10){
             //nuoc di cua quan tot
-            const direction = this.getValue() > 0 ? 1 : -1;
+            const direction = this.getValue() > 0 ? -1 : 1;
             if (!(startY==7&&direction==1)&&!(startY==0&&direction==-1)){
                 endY += direction;
             }
             for (let i=-1 ; i<=1 ; i+=2){
                 if (startX+i>=0 && startX+i<=7){
-                    if (boardState[endY][startX+i] != null && boardState[endY][startX+i].getValue()*this.getValue()<0){
-                        positiveMove.push({indexX: startX+i, indexY: endY});
+                    if (boardState[endY][startX+i].piece != null && boardState[endY][startX+i].piece.getValue() * this.getValue()<0){
+                        positiveMove.push({indexX: endY, indexY: startX + i});
                     }
                 }
             }
-            if (boardState[endY][startX] == null){
-                positiveMove.push({indexX: startX, indexY: endY});
+            if (boardState[endY][startX].piece == null){
+                positiveMove.push({indexX: endY, indexY: startX});
             }
             else return positiveMove;
-            if ((endY != 1&& endY != 6)||(endY == 1&&this.getValue()<0)||(endY == 6&&this.getValue()>0)){
+            if ((startY != 1&& startY != 6)||(startY == 1&&this.getValue() > 0)||(startY == 6&&this.getValue() < 0)){
                 return positiveMove;
             }
             endY += direction;
-            if (boardState[endY][startX] == null){
-                positiveMove.push({indexX: startX, indexY: endY});
+            if (boardState[endY][startX].piece == null){
+                positiveMove.push({indexX: endY, indexY: startX});
             }
             return positiveMove;
         }else if (Math.abs(this.getValue()) == 90){
@@ -40,7 +40,7 @@ export default class Pawn extends Piece {
             Queen.prototype.move(boardState, startX, startY);
 
         }
-
+        return positiveMove;
     }
 }
 
